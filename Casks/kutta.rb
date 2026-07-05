76532d33a75920ae95e929247843605d3bff5f98
crgimenes <crg@crg.eti.br>
2026-07-05 13:25:00 -0300

    kutta 0.1.6

--- Casks/kutta.rb
+++ Casks/kutta.rb
@@ -1,6 +1,6 @@
 cask "kutta" do
-  version "0.1.5"
-  sha256 "c867b4c1b0084d6d347bb2423538bb675ddc957a03fbfd7b84c09c8eeb80926b"
+  version "0.1.6"
+  sha256 "c9fcbfefb976425a3687745ca9477625884a2191771c8144e7bfcb2ab09a6ba4"
 
   url "https://github.com/crgimenes/kutta/releases/download/v#{version}/kutta-darwin-universal.zip"
   name "kutta"
