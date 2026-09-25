0980b6d9d85fc7d4effb00810a2d9502ddd469dd
crgimenes <crg@crg.eti.br>
2026-09-24 23:44:13 -0300

    gion 0.0.6

--- Casks/gion.rb
+++ Casks/gion.rb
@@ -1,6 +1,6 @@
 cask "gion" do
-  version "0.0.4"
-  sha256 "49cf588ef03c6d71222983b0b70c556e15c0c66510e71e41e41fc06f4bcadd2a"
+  version "0.0.6"
+  sha256 "d45d4e6b3dcc5ec91bfb998d3e63bce0017da083559e6e0045eadafc24207afb"
 
   url "https://github.com/crgimenes/gion/releases/download/v#{version}/gion-darwin-universal.zip"
   name "gion"
@@ -12,7 +12,7 @@ cask "gion" do
     strategy :github_latest
   end
 
-  depends_on macos: :monterey
+  depends_on macos: :ventura
 
   app "gion.app"
 end
